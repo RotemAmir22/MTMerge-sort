acac1c173cf951c902594a46307d97877a3d8a66
RotemAmir22 <100492679+RotemAmir22@users.noreply.github.com>
2023-04-30 10:56:20 +0300

    merge sort

--- MTMergeSort.cs
+++ MTMergeSort.cs
@@ -1,92 +1,75 @@
-﻿import Thread;
+﻿using System.Threading;
 
 public class MTMergeSort
 {
-        public List<(string t)> MergeSort(string[] strList, int nMin = 2)
+    static void Main(string[] args)
     {
-        int l = strList[0];
-        int r = strList.Length - 1;
-        int mid = (l + r) / 2;
-        // create new threads
-        Thread leftSide = new Thread(mergeSort(strList, l, mid));
-        Thread rightSide = new Thread(mergeSort(strList, mid + 1, r));
-        // run the threads
-        leftSide.Start();
-        rightSide.Start();
-        leftSide.Join();
-        rightSide.Join();
-        Merge(strList,l, mid,r);
+        string[] arr = { "3", "5", "1", "7", "4", "9", "8", "6", "2" };
+        arr = MergeSort(arr);
+        Console.WriteLine(string.Join(",", arr));
+    }
+
+
+    public List<string> MergeSort(string[] strList, int nMin = 2)
+    {
+        if (strList.Length < nMin) 
+        {
+            Merge(strList, strList, string[]);
+            List<string> list = strList.ToList();
+            return list;
+        }
+
+
+        int mid = strList.Length / 2;
+
+        // create left and right sub-arrays
+        string[] left = new int[mid];
+        string[] right = new int[strList.Length - mid];
+        Array.Copy(strList, 0, left, 0, mid);
+        Array.Copy(strList, mid, right, 0, strList.Length - mid);
+
+        // create threads to sort each sub-array
+        Thread leftThread = new Thread(() => MergeSort(left));
+        Thread rightThread = new Thread(() => MergeSort(right));
+
+        // start threads and wait for them to complete
+        leftThread.Start();
+        rightThread.Start();
+        leftThread.Join();
+        rightThread.Join();
+
+        // merge the sorted sub-arrays
+        Merge(strList, left, right);
+        
         
     }
 
-    static void mergeSort(string[] A, int l, int r)
+    static void Merge(string[] arr, string[] left, string[] right) 
     {
-        if (A.Length < 2 || l > r)
-            return;
-        int m = (l + r) / 2;
-        // create new threads
-        Thread leftSide = new Thread(mergeSort(strList, l, m));
-        Thread rightSide = new Thread(mergeSort(strList, m + 1, r));
-        // run the threads
-        leftSide.Start();
-        rightSide.Start();
-        leftSide.Join();
-        rightSide.Join();
+        int i = 0, j = 0, k = 0;
 
+        // merge left and right sub-arrays into arr
+        while (i < left.Length && j < right.Length) 
+        {
+            if (left[i] < right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+
+        // copy any remaining elements from left and right
+        while (i < left.Length) 
+        {
+            arr[k++] = left[i++];
+        }
+        while (j < right.Length) 
+        {
+            arr[k++] = right[j++];
+        }
     }
 
-    static void Merge(string[] A, int l, int m, int r)
-    {
-        // Create L ← A[p..q] and M ← A[q+1..r]
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        int L[n1], M[n2];
-
-        for (int i = 0; i < n1; i++)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; j++)
-            M[j] = arr[m + 1 + j];
-
-        // Maintain current index of sub-arrays and main array
-        int i, j, k;
-        i = 0;
-        j = 0;
-        k = l;
-
-        // Until we reach either end of either L or M, pick larger among
-        // elements L and M and place them in the correct position at A[p..r]
-        while (i < n1 && j < n2)
-        {
-            if (L[i] <= M[j])
-            {
-                arr[k] = L[i];
-                i++;
-            }
-            else
-            {
-                arr[k] = M[j];
-                j++;
-            }
-            k++;
-        }
-
-        // When we run out of elements in either L or M,
-        // pick up the remaining elements and put in A[p..r]
-        while (i < n1)
-        {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2)
-        {
-            arr[k] = M[j];
-            j++;
-            k++;
-        }
-
-    }
+    
+}
 
 
